b3747584674ab11c62d2e34d89f18f86d1c168ca
Amir <a.h.matloubi@gmail.com>
2025-10-27 10:21:06 +0330

    Didgah: Add project

---
+++ BluProject/BluProject/BluProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluProjectApp.swift
+//  BluProject
+//
+//  Created by Amir Hossein on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BluProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
